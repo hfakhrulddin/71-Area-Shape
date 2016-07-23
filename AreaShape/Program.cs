5c085994ba1761714670bb2e549cf2326360d010
Hussen Fakhrulddin <hf556482@gmail.com>
2016-07-22 23:28:41 -0700

    init

---
+++ AreaShape/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes =
+        {
+            new Square(5, "Square #1"),
+            new Circle(3, "Circle #1"),
+            new Rectangle( 4, 5, "Rectangle #1")
+        };
+
+            System.Console.WriteLine("Shapes Collection");
+            foreach (Shape s in shapes)
+            {
+                System.Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AreaShape/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaShape
+{
+    public abstract class Shape
+    {
+        private string myId;
+
+        public Shape(string s)
+        {
+            Id = s;   // calling the set accessor of the Id property
+        }
+
+        public string Id
+        {
+            get
+            {
+                return myId;
+            }
+
+            set
+            {
+                myId = value;
+            }
+        }
+
+        // Area is a read-only property - only a get accessor is needed:
+        public abstract double Area
+        {
+            get;
+        }
+
+        public override string ToString()
+        {
+            return Id + " Area = " + string.Format("{0:F2}", Area);
+        }
+    }
+}
